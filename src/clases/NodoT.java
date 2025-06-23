931d9008e0780d9b1dbdb8ab7ab694fdbedfadc6
SChapmanL <sebastianchapman0205@gmail.com>
2025-06-22 23:01:28 -0500

    arrayList y Tramite, NodoT y ColaT modificados

--- src/clases/NodoT.java
+++ src/clases/NodoT.java
@@ -1,50 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package clases;
 
 /**
  *
  * @author ariel
  */
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 public class NodoT {
-    String h_inicio , h_final,dependencias;
-    boolean docs;
-    boolean atendido;
-    Expediente exp;
+    tramite Tramite;
     NodoT sig;
     NodoT prev;
 
-    public NodoT(String dependencias, boolean docs, Expediente exp,boolean atendido) {
-        this.dependencias = dependencias;
-        this.docs = docs;
-        this.exp = exp;
-        this.atendido = atendido;
-        InicioTramite();
+    public NodoT(tramite Tramite) {
+        this.Tramite = Tramite;
+        this.sig = null;
+        this.prev = null;
     }
-    
-    public void InicioTramite() {
-        // Obtener fecha y hora actual
-        LocalDateTime fechaHoraActual = LocalDateTime.now();
 
-        // Formateador personalizado (puedes cambiar el formato si deseas)
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-
-        // Coloar la Hora de inicio del tramite
-        h_inicio= fechaHoraActual.format(formato);
+    // Getter and setters
+    public NodoT getSig() {
+        return sig;
     }
-    
-    public void FinalTramite(){
-        // Obtener fecha y hora actual
-        LocalDateTime fechaHoraActual = LocalDateTime.now();
 
-        // Formateador personalizado (puedes cambiar el formato si deseas)
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-
-        // Coloar la Hora de inicio del tramite
-        h_final= fechaHoraActual.format(formato);
+    public void setSig(NodoT sig) {
+        this.sig = sig;
     }
+
+    public NodoT getPrev() {
+        return prev;
+    }
+
+    public void setPrev(NodoT prev) {
+        this.prev = prev;
+    }
+
 }
